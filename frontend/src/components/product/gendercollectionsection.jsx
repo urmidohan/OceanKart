d23546175e91adada050c41465b6ad7fa2bc9317
Urmi <urmidohan@gmail.com>
2025-06-26 01:45:59 +0530

    Fix image path1

--- frontend/src/components/product/gendercollectionsection.jsx
+++ frontend/src/components/product/gendercollectionsection.jsx
@@ -7,7 +7,7 @@ const GenderCollectionSection = () => {
         <div className="container mx-auto flex flex-col md:flex-row gap-8">
             {/* Women's collection */}
             <div className="relative flex-1">
-                <img src="frontend\images\womensection.jpg" alt="Women's Collection" className="w-full h-[500px] object-cover" />
+                <img src="/womensection.jpg" alt="Women's Collection" className="w-full h-[500px] object-cover" />
                 <div className="absolute bottom-8 left-8 bg-white bg-opacity-90 p-4">
                     <h2 className="text-2xl font-bold text-gray-900 mb-3"> Women's Collection</h2>
                     <Link to="/collections/all?gender=Women" className="text-gray-900 underline">Shop Now</Link>
@@ -15,7 +15,7 @@ const GenderCollectionSection = () => {
             </div>
              {/* Men's collection */}
              <div className="relative flex-1">
-                <img src="frontend\images\mensection.jpg" alt="Men's Collection" className="w-full h-[500px] object-cover" />
+                <img src="/mensection.jpg" alt="Men's Collection" className="w-full h-[500px] object-cover" />
                 <div className="absolute bottom-8 left-8 bg-white bg-opacity-90 p-4">
                     <h2 className="text-2xl font-bold text-gray-900 mb-3"> Men's Collection</h2>
                     <Link to="/collections/all?gender=Men" className="text-gray-900 underline">Shop Now</Link>
